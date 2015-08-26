b232c97f9d4404015665578b61b4a40dd9accb80
Andrew <svc_nativeapps@github.dowjones.net>
2015-08-26 13:27:14 -0400

    added build-config

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,6 @@
-var GitSync = require('./node_modules/git-s3-sync/');
+var GitSync = require('./node_modules/GitSync/Gulpfile.js');
 var path = require('path');
 
 module.exports = function(gulp){
-  GitSync(gulp);
-  gulp.task('git-s3-sync', function(){
-    //to be configured
-    return gulp.src('src/js/*.js');
-  });
+  var testPath = process.env(path);
 }

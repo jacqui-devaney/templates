99c1d48786a95f6e185cf8a1108fffd685c4f1e9
Roben Kleene <roben.kleene@dowjones.com>
2016-04-19 16:11:49 -0400

    Start setting up vertical styles

--- grid_builder.js
+++ grid_builder.js
@@ -53,20 +53,68 @@ function grid_builder() {
           // Computed
         }
       }
+    },
+    vertical: {
+      portrait: {
+        margin: {
+          top: 8,
+          bottom: 8
+        },
+        lineHeight: 20,
+        screen: {
+          height: 768
+        },
+        grid: {
+          // Computed
+        }
+      },
+      landscape: {
+        margin: {
+          top: 8,
+          bottom: 8
+        },
+        lineHeight: 20,
+        screen: {
+          width: 1024
+        },
+        grid: {
+          // Computed
+        }
+      }
     }
   };
 
+
   // Computed
+
+  // Vertical
+  
+  // Grid width (screen size minus the top and bottom margins)
+  this.grid.vertical.portrait.grid.height = this.grid.vertical.portrait.screen.height - this.grid.vertical.portrait.margin.top - this.grid.vertical.portrait.margin.bottom;
+  this.grid.vertical.landscape.grid.height = this.grid.vertical.landscape.screen.height - this.grid.vertical.landscape.margin.top - this.grid.vertical.landscape.margin.bottom;
+
+  // Lines count
+  this.grid.vertical.portrait.lines = this.grid.vertical.portrait.grid.height / this.grid.vertical.portrait.lineHeight;
+  this.grid.vertical.landscape.lines = this.grid.vertical.landscape.grid.height / this.grid.vertical.landscape.lineHeight;
+
+  // Horizontal
+
+  // Grid width (screen size minus the left and right margins)
   this.grid.horizontal.portrait.grid.width = this.grid.horizontal.portrait.screen.width - this.grid.horizontal.portrait.margin.left - this.grid.horizontal.portrait.margin.right;
   this.grid.horizontal.landscape.grid.width = this.grid.horizontal.landscape.screen.width - this.grid.horizontal.landscape.margin.left - this.grid.horizontal.landscape.margin.right;
 
+  // Column Width (width of each column, the "unit")
   this.grid.horizontal.portrait.column.width = (this.grid.horizontal.portrait.grid.width - (this.grid.horizontal.portrait.columns - 1) * this.grid.horizontal.portrait.gutter) / this.grid.horizontal.portrait.columns;
   this.grid.horizontal.landscape.column.width = (this.grid.horizontal.landscape.grid.width - (this.grid.horizontal.landscape.columns - 1) * this.grid.horizontal.landscape.gutter) / this.grid.horizontal.landscape.columns;
 }
 
 module.exports = grid_builder;
 
-grid_builder.prototype.widthForPortraitColumnCount = function(columnCount) {
+// Vertical
+
+// Horizontal
+
+grid_builder.prototype.portraitWidthForColumns = function(columnCount) {
   var baseWidth = this.grid.horizontal.portrait.column.width * columnCount;
   var gutterWidthSum = this.grid.horizontal.portrait.gutter * (columnCount - 1);
 
@@ -74,34 +122,61 @@ grid_builder.prototype.widthForPortraitColumnCount = function(columnCount) {
   return totalWidth;
 };
 
-grid_builder.prototype.widthForLandscapeColumnCount = function(columnCount) {
+grid_builder.prototype.landscapeWidthForColumns = function(columnCount) {
   var baseWidth = this.grid.horizontal.landscape.column.width * columnCount;
   var gutterWidthSum = this.grid.horizontal.portrait.gutter * (columnCount - 1);
   var totalWidth = baseWidth + gutterWidthSum;
   return totalWidth;
 };
 
-grid_builder.prototype.positionForPortraitPositionIndex = function(positionIndex) {
+grid_builder.prototype.portraitPositionXForIndex = function(positionIndex) {
   var startingValue = this.grid.horizontal.portrait.margin.left;
 
   if (positionIndex <= 1) {
     return startingValue;
   }
 
-  var finalPosition = startingValue + this.widthForPortraitColumnCount(positionIndex - 1) + this.grid.horizontal.portrait.gutter;
+  var finalPosition = startingValue + this.portraitWidthForColumns(positionIndex - 1) + this.grid.horizontal.portrait.gutter;
 
   return finalPosition;
 };
 
-grid_builder.prototype.positionForLandscapePositionIndex = function(positionIndex) {
+grid_builder.prototype.landscapePositionXForIndex = function(positionIndex) {
   var startingIndex = this.grid.horizontal.landscape.margin.left;
 
   if (positionIndex <= 1) {
     return startingIndex;
   }
 
-  var finalPosition = startingIndex + this.widthForLandscapeColumnCount(positionIndex - 1) + this.grid.horizontal.landscape.gutter;
+  var finalPosition = startingIndex + this.landscapeWidthForColumns(positionIndex - 1) + this.grid.horizontal.landscape.gutter;
   return finalPosition;
+};
+
+grid_builder.prototype.getVerticalStyles = function() {
+  var styles = {};
+
+  // Position
+  for (var i = 1; i <= this.grid.vertical.landscape.lines; i++) {
+    styles[styleTag([COLUMNS_STYLE_COMPONENT, i])] = {
+      "top": {
+        if: "=portrait",
+        then: this.portraitWidthForColumns(i),
+        else: this.landscapeWidthForColumns(i)
+      }
+    };
+  }
+
+  // Lines
+  for (var i = 1; i <= this.grid.vertical.landscape.lines; i++) {
+    styles[styleTag([COLUMNS_STYLE_COMPONENT, i])] = {
+      "height": {
+        if: "=portrait",
+        then: this.portraitWidthForColumns(i),
+        else: this.landscapeWidthForColumns(i)
+      }
+    };
+  }
+
 };
 
 grid_builder.prototype.getHorizontalStyles = function() {
@@ -115,22 +190,22 @@ grid_builder.prototype.getHorizontalStyles = function() {
     styles[styleTag([COLUMNS_STYLE_COMPONENT, i])] = {
       "width": {
         if: "=portrait",
-        then: this.widthForPortraitColumnCount(i),
-        else: this.widthForLandscapeColumnCount(i)
+        then: this.portraitWidthForColumns(i),
+        else: this.landscapeWidthForColumns(i)
       }
     };
     // Portrait
     styles[styleTag([PORTRAIT_STYLE_COMPONENT, COLUMNS_STYLE_COMPONENT, i])] = {
       "width": {
         if: "=portrait",
-        then: this.widthForPortraitColumnCount(i)
+        then: this.portraitWidthForColumns(i)
       }
     };
     // Landscape
     styles[styleTag([LANDSCAPE_STYLE_COMPONENT, COLUMNS_STYLE_COMPONENT, i])] = {
       "width": {
         if: "=landscape",
-        then: this.widthForLandscapeColumnCount(i)
+        then: this.landscapeWidthForColumns(i)
       }
     };
 
@@ -139,22 +214,22 @@ grid_builder.prototype.getHorizontalStyles = function() {
     styles[styleTag([XPOSITION_STYLE_COMPONENT, i])] = {
       left: {
         if: "=portrait",
-        then: this.positionForPortraitPositionIndex(i),
-        else: this.positionForLandscapePositionIndex(i)
+        then: this.portraitPositionXForIndex(i),
+        else: this.landscapePositionXForIndex(i)
       }
     };
    // Portrait
     styles[styleTag([PORTRAIT_STYLE_COMPONENT, XPOSITION_STYLE_COMPONENT, i])] = {
       left: {
         if: "=portrait",
-        then: this.positionForPortraitPositionIndex(i)
+        then: this.portraitPositionXForIndex(i)
       }
     };
     // Landscape
     styles[styleTag([LANDSCAPE_STYLE_COMPONENT, XPOSITION_STYLE_COMPONENT, i])] = {
       left: {
         if: "=landscape",
-        then: this.positionForLandscapePositionIndex(i)
+        then: this.landscapePositionXForIndex(i)
       }
     };
   }
